6ec9cb793df3c000e632a03dda2db230f6e35943
yuying_xixi <3879941788@qq.com>
2026-05-11 00:45:46 +0800

    完善

--- Global E-commerce/enter_practice.py
+++ Global E-commerce/enter_practice.py
@@ -1,8 +1,8 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Page, BrowserContext
 import time
 
 # 屏幕点击实现及调试
-def debug_click(page, context, x, y):
+def debug_click(page: Page, context: BrowserContext, x: int, y: int) -> Page:
     """
     在指定的 x, y 坐标模拟点击，并立即在该位置显示一个红点调试动画。
     :param page :playwright单页面对象 : 用于控制和操作浏览器中单个页面的对象
@@ -60,7 +60,7 @@ def debug_click(page, context, x, y):
         return page
 
 # 登录功能
-def login(page, username, password):
+def login(page: Page, username: int, password: int) -> None:
     """
     给定学生的用户名和密码完成登录操作,并返回登录后的页面对象
     :param page: 页面对象
@@ -78,7 +78,7 @@ def login(page, username, password):
     print(f"成功登录学号: {username}")
 
 # 进入实验主页面
-def enter_practice(page, context):
+def enter_practice(page: Page, context: BrowserContext) -> Page:
     """
      进入实验主页
     :param page: 页面对象
@@ -114,7 +114,7 @@ def enter_practice(page, context):
     return new_page
 
 # 选择实验并进入
-def choose_project_task(page, context, project_number, task_number):
+def choose_project_task(page: Page, context: BrowserContext, project_number: int, task_number: int):
     """
     选择进入对应的实验以及实验项目
     :param page:
@@ -123,43 +123,75 @@ def choose_project_task(page, context, project_number, task_number):
     :param task_number: int: 项目编号
     :return: 进入习题页面的网址
     """
-    # 1. 定位大的项目块 (项目一、项目二等)
+    # 定位大的项目块 (项目一、项目二等)
     project = page.locator(".rationalism-left-item").nth(project_number - 1)
 
-    # 2. 定位任务块
-    # 在 project 内部寻找第 N 个任务主容器
+    # 定位任务块(任务一,任务二)
     task_main = project.locator(".rationalism-left-task-main").nth(task_number - 1)
 
     # 3. 定位具体的任务文字所在的 div
     task = task_main.locator(".rationalism-left-task")
 
-    # --- 增加等待和可见性检查 ---
     # 有些任务可能是隐藏的，需要滚动并等待
     task.scroll_into_view_if_needed()
 
     # 强制等待元素可以被点击（防止动画干扰）
     task.wait_for(state="visible", timeout=10000)
 
-    # 点击任务
+    # 选择任务块
     task.click()
 
+    # 等待进入实验按钮加载
     time.sleep(3)
 
+    # 点击进入实验
     with context.expect_page() as new_page_info:
         print("进行点击")
         debug_click(page, context, 720, 210)
+    if new_page_info:
+        print("进入实验选择页面成功")
+        return new_page_info
+    return None
+
+
+def start_practice(page, context, choose_practice_index, practice_number=2):
+    """
+    选择训练目标
+    :param page: page对象: 接收对象
+    :param context: 上下文对象: 接收对象
+    :param choose_practice_index: int: 选择第几个训练模块(1, 2, 3)
+    :param practice_number: int: 页面有几个训练模块(2, 3)
+    :return:
+    """
+
+    if practice_number not in {2, 3}:
+        print(f"页面训练模块数量错误,不存在{practice_number}块")
+        exit()
+    if choose_practice_index > practice_number:
+        print(f"不存在第{choose_practice_index}个训练模块")
+        exit()
+
 
     # 获取新打开的页面对象
-    new_page = new_page_info.value
+    new_page = page.value
 
     # 注意
     # 若进入实验动画渲染时间太长可适当增加sleep时间
     # 注意
     time.sleep(20)
 
-    # 两个选项
-    debug_click(new_page, context, 580, 680)
-    # debug_click(new_page, context, 700, 680)
+    if practice_number == 2:
+        if choose_practice_index == 1:
+            new_page = debug_click(new_page, context, 580, 680)
+        else:
+            new_page = debug_click(new_page, context, 700, 680)
+    else:
+        if choose_practice_index == 1:
+            new_page = debug_click(new_page, context, 500, 680)
+        elif choose_practice_index == 2:
+            new_page = debug_click(new_page, context, 650, 680)
+        else:
+            new_page = debug_click(new_page, context, 780, 680)
 
     return new_page
 
@@ -177,9 +209,13 @@ def run():
         # 返回实验主页
         home_page = enter_practice(page, context)
 
-        lab_page = choose_project_task(home_page, context, 2, 2)
+        lab_page = choose_project_task(home_page, context, 4, 4)
 
-        print(lab_page)
+        if lab_page is not None:
+            start_practice(lab_page, context,3, 3)
+            print("进入实验选择成功")
+        else:
+            print("进入实验选择页面失败")
 
         time.sleep(100)
         browser.close()
